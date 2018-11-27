55a28e4a01e3cf8016350b0e224b26e36fe6afc0
AndreiYT <45375137+AndreiYT@users.noreply.github.com>
2018-11-27 11:31:30 +0800

    Create Activity_A_bejerano.py

---
+++ Activity_A_bejerano.py
@@ -0,0 +1,16 @@
+for x in range(1, 17, 3):
+    print(x, end='\t')
+print()
+
+for x in range(100, 78, -7):
+    print(x, end='\t')
+print()
+
+for x in range(0, 2):
+    print(x, end='\t')
+
+    if x % 1 == 0:
+        print(0, end='\t')
+    else:
+        print(x, end='\t')
+print()
