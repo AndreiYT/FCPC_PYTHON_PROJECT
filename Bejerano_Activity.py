06b674ef4096ae92ea37b05fdd06000b57eb60d4
AndreiYT <45375137+AndreiYT@users.noreply.github.com>
2019-01-29 11:01:05 +0800

    Create Bejerano_Activity.py

---
+++ Bejerano_Activity.py
@@ -0,0 +1,18 @@
+user=input("Enter Username: ")
+password=input("Enter password: ")
+
+if (len(user)>=8):
+    if(user=="andresaa"):
+        if(len(password)>=8):
+            if(password=="pogiako2"):
+                print("Login Success")
+            else:
+                print("Eeennnkkk")
+        else:
+            (len(password)>8) | (len(password)<8)
+            print("Eeennnkkk")
+    else:
+        print("Eeennnkkk")
+else:
+    (len(user)>8) | (len(user)<8)
+    print("Eeennnkkk")
